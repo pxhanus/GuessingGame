bcb7d2677dee676b09e5499244e51cc4cb86e2b1
pxhanus <pxhanus@gmail.com>
2015-11-05 13:52:22 -0600

    first commit

---
+++ GuessingGame/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblPrompt = New System.Windows.Forms.Label
+        Me.btnCheckGuess = New System.Windows.Forms.Button
+        Me.txtPlayerGuess = New System.Windows.Forms.TextBox
+        Me.lblMessage = New System.Windows.Forms.Label
+        Me.SuspendLayout()
+        '
+        'lblPrompt
+        '
+        Me.lblPrompt.AutoSize = True
+        Me.lblPrompt.Location = New System.Drawing.Point(40, 44)
+        Me.lblPrompt.Name = "lblPrompt"
+        Me.lblPrompt.Size = New System.Drawing.Size(167, 13)
+        Me.lblPrompt.TabIndex = 0
+        Me.lblPrompt.Text = "Enter a guess between 1 and 50: "
+        '
+        'btnCheckGuess
+        '
+        Me.btnCheckGuess.Location = New System.Drawing.Point(103, 138)
+        Me.btnCheckGuess.Name = "btnCheckGuess"
+        Me.btnCheckGuess.Size = New System.Drawing.Size(83, 23)
+        Me.btnCheckGuess.TabIndex = 1
+        Me.btnCheckGuess.Text = "Check Guess"
+        Me.btnCheckGuess.UseVisualStyleBackColor = True
+        '
+        'txtPlayerGuess
+        '
+        Me.txtPlayerGuess.Location = New System.Drawing.Point(213, 41)
+        Me.txtPlayerGuess.Name = "txtPlayerGuess"
+        Me.txtPlayerGuess.Size = New System.Drawing.Size(40, 20)
+        Me.txtPlayerGuess.TabIndex = 2
+        '
+        'lblMessage
+        '
+        Me.lblMessage.AutoSize = True
+        Me.lblMessage.Location = New System.Drawing.Point(120, 94)
+        Me.lblMessage.Name = "lblMessage"
+        Me.lblMessage.Size = New System.Drawing.Size(0, 13)
+        Me.lblMessage.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 173)
+        Me.Controls.Add(Me.lblMessage)
+        Me.Controls.Add(Me.txtPlayerGuess)
+        Me.Controls.Add(Me.btnCheckGuess)
+        Me.Controls.Add(Me.lblPrompt)
+        Me.Name = "Form1"
+        Me.Text = "Guessing Game"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblPrompt As System.Windows.Forms.Label
+    Friend WithEvents btnCheckGuess As System.Windows.Forms.Button
+    Friend WithEvents txtPlayerGuess As System.Windows.Forms.TextBox
+    Friend WithEvents lblMessage As System.Windows.Forms.Label
+
+End Class
---
+++ GuessingGame/Form1.vb
@@ -0,0 +1,24 @@
+﻿'Patricia Hanus
+'150922
+'Guessing Game
+'______________________________________________________________________________________________________
+
+Public Class Form1
+    Const MIN As Integer = 1
+    Const MAX As Integer = 50
+
+    Private Sub btnCheckGuess_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnCheckGuess.Click
+        Randomize()
+        Static secretNumber As Integer = Int((MAX - MIN + 1) * Rnd() + MIN)
+        Dim guess As Integer
+
+        guess = Val(Me.txtPlayerGuess.Text)
+        If guess = secretNumber Then        'Correct
+            MessageBox.Show("You guessed it!")
+        ElseIf guess < secretNumber Then    'Too low
+            MessageBox.Show("Too low.")
+        ElseIf guess > secretNumber Then    'Too high
+            MessageBox.Show("Too high.")
+        End If
+    End Sub
+End Class
